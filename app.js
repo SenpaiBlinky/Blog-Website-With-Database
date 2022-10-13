10a0d9e921a5f669d0cd6a919ebd53ca8faaf005
Master <blinkerion@gmail.com>
2022-10-13 00:41:49 -0500

    final

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 //jshint esversion:6
 
-// we can use npm install to automatically install all npm modules required for the project (cool!)
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
-// used for browser extensions with spaces such as tolio-batkatov or tolio batkatov
-const _ = require("lodash")
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,71 +11,70 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-let posts = []
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/blogDB");
 
+const postSchema = {
+  title: String,
+  content: String
+};
 
-// home page
-app.get("/", function(req, res) {
+const Post = mongoose.model("Post", postSchema);
 
-  res.render("home", {homeStartingContent: homeStartingContent, posts: posts})
-})
+app.get("/", function(req, res){
 
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
 
-// about page
-app.get("/about", function(req, res) {
-
-  res.render("about", {aboutContent: aboutContent})
-})
-
-
-// contact page
-app.get("/contact", function(req, res) {
-
-  res.render("contact", {contactContent: contactContent})
-})
-
-// compose page
-app.get("/compose", function(req, res) {
-
-  res.render("compose")
-})
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
 
 app.post("/compose", function(req, res){
-  const post = {
-    titleText :req.body.titleText,
-    newText :req.body.newText
-  }
-  posts.push(post)
-
-  res.redirect("/")
-})
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
 
 
-
-app.get("/posts/:postName", function(req, res){
-  // uses the library to make the text lower case and also remove andy - or spaces for us :)
-  let postName = _.lowerCase(req.params.postName) 
-
-  posts.forEach(function(post){
-    if (_.lowerCase(post.titleText) == postName) {
-      res.render("post", {
-        title: post.titleText,
-        content: post.newText
-      })
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
     }
-  })
-})
+  });
+});
 
+app.get("/posts/:postId", function(req, res){
 
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
-
